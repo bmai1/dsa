3faf63eca68e31bd98adedc2c1c51f1fdb68a9dc
Brian Mai <0hrs3g@gmail.com>
2023-01-31 10:14:03 -0800

    Create 566_reshapematrix.cpp

---
+++ 566_reshapematrix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        // invalid size
+        if (r * c != mat.size() * mat[0].size()) { return mat; }
+        vector<vector<int>> ans;
+        vector<int> row;
+        // iterate through mat rows
+        for (auto r : mat) {
+            for (int n : r) {
+                row.push_back(n);
+                // push back correct row size
+                if (row.size() == c) { ans.push_back(row); row.clear(); }
+            }
+        }
+        return ans;
+    }
+};
