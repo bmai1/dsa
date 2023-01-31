8a0ed2e8ce85cc2213ea3231f7db7eaa746427b4
Brian Mai <0hrs3g@gmail.com>
2023-01-31 12:00:36 -0800

    str.find() rather than hash map, unoptimized

--- 1832_pangram.cpp
+++ 1832_pangram.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
+        // pangrams contain every letter in alphabet
         string alpha = "abcdefghijklmnopqrstuvwxyz";
         bool check = true;
         for (int i = 0; i < 26; ++i) {
