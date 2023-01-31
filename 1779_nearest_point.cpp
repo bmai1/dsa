b5e2dfba1eb0cda2a2e114c86388407d6cf718b4
Brian Mai <0hrs3g@gmail.com>
2023-01-31 12:03:21 -0800

    Manhattan distance of points sharing x or y-coord

--- 1779_nearest_point.cpp
+++ 1779_nearest_point.cpp
@@ -4,6 +4,7 @@ public:
         int min = INT_MAX, ans = -1; 
         for (int i = 0; i < points.size(); ++i) {
             if (points[i][0] != x && points[i][1] != y) { continue; }
+            // manhattan distance between two points = abs(x2 - x1) + abs(y2 - y1)
             int d = abs(x - points[i][0]) + abs(y - points[i][1]);
             if (d == 0) { return i; }
             else if (d < min) { 
