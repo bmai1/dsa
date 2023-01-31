e6e94f8fe7f68e09b15336d6415a541203e08a93
Brian Mai <0hrs3g@gmail.com>
2023-01-30 23:36:10 -0800

    Create bst_traversal.cpp

---
+++ bst_traversal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    void tr(TreeNode* root, vector<int>& ans) {
+        if (root == nullptr) { return; }
+        ans.push_back(root->val); // preorder, Input: root = [1,null,2,3], Output: [1,2,3]
+        tr(root->left, ans);
+        // ans.push_back(root->val); // inorder, Output: [1,3,2]
+        tr(root->right, ans);
+        // ans.push_back(root->val); // postorder, Output: [3,2,1]
+    }
+
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        tr(root, ans);
+        return ans;
+    }
+};
