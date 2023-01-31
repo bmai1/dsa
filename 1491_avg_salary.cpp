e48cd892a37b7eb25de89049c49cf389d879de8d
Brian Mai <0hrs3g@gmail.com>
2023-01-31 12:02:07 -0800

    O(n) brute force, find min/max then average

--- 1491_avg_salary.cpp
+++ 1491_avg_salary.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
+        // find min and max salary
         double max = 0, min = INT_MAX;
         for (int i = 0; i < salary.size(); ++i) {
             if (salary[i] > max) { max = salary[i]; }
             if (salary[i] < min) { min = salary[i]; }
         }
         
+        // calculate average
         double average = 0;
         for (int j = 0; j < salary.size(); ++j) {
             if (salary[j] != max && salary[j] != min) { average += salary[j]; }
