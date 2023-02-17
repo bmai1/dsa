bc383d4daf9b48cd50b94814874835e7653743e3
Brian Mai <0hrs3g@gmail.com>
2023-02-17 11:07:58 -0800

    more dp

---
+++ 64_min_path_sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, grid[0][0]));
+        // sum setup --> how?
+        for (int i = 1; i < m; ++i) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < n; ++j) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        // dp
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
