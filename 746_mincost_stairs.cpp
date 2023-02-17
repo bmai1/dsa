c62a3d1b8c0abbc284012230bf247a6013ebe3cf
Brian Mai <0hrs3g@gmail.com>
2023-02-16 17:12:28 -0800

    more dp practice hoorah..

---
+++ 746_mincost_stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n);
+        for (int i = 0; i < n; ++i) {
+            // start point
+            if (i < 2) { dp[i] = cost[i]; }
+            else {
+                dp[i] = cost[i] + min(dp[i-1], dp[i-2]);
+            }
+        }
+        return min(dp[n-1], dp[n-2]);
+    }
+};
