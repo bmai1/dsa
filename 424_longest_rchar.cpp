dcaa736fdfc85d5be7f87c2e333a99c1e5b7b170
Brian Mai <0hrs3g@gmail.com>
2023-02-20 03:55:44 -0800

    chatGPT max sliding window length

--- 424_longest_rchar.cpp
+++ 424_longest_rchar.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         // maxCount of substr so far, maxLength of window
-        int n = s.size(), left = 0, maxCount = 1, maxLength = 0;
+        int left = 0, maxCount = 1, maxLength = 0;
         vector<int> freq(26, 0);
-        for (int right = 0; right < n; ++right) {
+        for (int right = 0; right < s.size(); ++right) {
             freq[s[right] - 'A']++;
             maxCount = max(maxCount, freq[s[right] - 'A']);
             if (right - left + 1 - maxCount > k) {
