bb11fd4519ecf19daffb330a1f2869fd08e7f3b6
Brian Mai <0hrs3g@gmail.com>
2023-05-03 13:04:43 -0700

    string gcd is a thing ? euclidean gcd function

--- 1071_stringgcd.cpp
+++ 1071_stringgcd.cpp
@@ -1,6 +1,16 @@
 class Solution {
+private:
+    // euclidean naive gcd ?
+    int euclidean(int a, int b) {
+        int gcd = 0;
+        int s = min(a, b);
+        for (int i = s; i >= 2; --i) {
+            if (a % i == 0 && b % i == 0) { return i; }
+        }
+        return 1;
+    }
 public:
     string gcdOfStrings(string str1, string str2) {
-        return (str1 + str2 == str2 + str1) ? str1.substr(0, gcd(size(str1), size(str2))) : "";
+        return (str1 + str2 == str2 + str1) ? str1.substr(0, euclidean(size(str1), size(str2))) : "";
     }
 };
