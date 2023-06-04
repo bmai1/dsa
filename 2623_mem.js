e5896c867fa72bf005e7ca80459c80ee35c629ba
Brian Mai <0hrs3g@gmail.com>
2023-06-04 02:27:24 -0700

    js cache

---
+++ 2623_mem.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        const key = String(args);
+        if (key in cache) { return cache[key]; }
+        cache[key] = fn(...args);
+        return cache[key];
+    }
+}
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
