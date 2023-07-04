3b4675fcc8f679c2008b8265bdecf733e1dbe543
Brian Mai <0hrs3g@gmail.com>
2023-07-04 06:16:04 -0700

    standard bfs queue

---
+++ 199_rightview.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) return {};
+        vector<int> ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int n = q.size();
+            for (int i = 0; i < n; ++i) {
+                TreeNode* node = q.front(); q.pop();
+                if (i == n - 1) {
+                    ans.push_back(node->val);
+                }
+                // why does left come before?
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return ans;
+    }
+};
