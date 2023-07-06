6676860dfe1954af97d70765decf5be6f0ba6990
Brian Mai <0hrs3g@gmail.com>
2023-07-06 11:24:16 -0700

    sexy

--- 459_strpat.py
+++ 459_strpat.py
@@ -1,3 +1,6 @@
 class Solution(object):
     def repeatedSubstringPattern(self, s):
         return s in (2 * s)[1:-1]
+
+        # Consider a string S="helloworld". Now, given another string T="lloworldhe".
+        # Can we figure out if T is a rotated version of S? Yes, we can! We check if S is a substring of T+T.
