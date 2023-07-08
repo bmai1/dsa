393be02c5f9a787d48e1c31a1ecfaa998feb4775
Brian Mai <0hrs3g@gmail.com>
2023-07-08 11:49:59 -0700

    unordered set size to cnt unique

--- 804_morse.cpp
+++ 804_morse.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
         vector<string> alpha = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-        set<string> s;
+        unordered_set<string> s;
         for (auto word : words) {
             string morse;
             for (auto c : word) {
