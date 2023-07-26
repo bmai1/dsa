db0162a8989ec5f12ab0075a318d98e647473dca
Brian Mai <0hrs3g@gmail.com>
2023-07-26 16:02:16 -0700

    concise array count consecutives

--- 485_consec.cpp
+++ 485_consec.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
         int ans = 0, curr = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i]) ans = max(++curr, ans);
+        for (int n : nums) {
+            if (n) ans = max(++curr, ans);
             else curr = 0;
         }
         return ans;
