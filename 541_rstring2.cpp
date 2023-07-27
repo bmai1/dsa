14d4d8f402a8ecc1b1a4b8f3823649660cef9ebc
Brian Mai <0hrs3g@gmail.com>
2023-07-27 14:48:56 -0700

    two-pointer based approach commented

--- 541_rstring2.cpp
+++ 541_rstring2.cpp
@@ -23,5 +23,22 @@ public:
             start += 2 * k;
         }
         return ans;  
+
+        string ans = "";
+        int n = s.size() - 1; // last index before overflow
+        int i = 0; 
+        for (;;) {
+            // move j forward k characters (go to end if <k remain)
+            int j = min(n, i + k - 1);
+            // append s[j..i] to answer (reversed s[i..j])
+            for (j; j >= i; --j) ans += s[j];
+            // append s[i+k..i+(2*k)] to answer
+            for (int l = i + k; l < i + 2 * k; ++l) {
+                if (l > n) return ans; // finished reversal
+                ans += s[l];
+            }
+            i += 2 * k; // new start index
+        }
+        return "cum";  
     }
 };
