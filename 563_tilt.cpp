66cc5f39c4f95d904dabacf58beea9050758f82b
Brian Mai <0hrs3g@gmail.com>
2023-07-27 17:24:35 -0700

    concise recursion sum binary tree

--- 563_tilt.cpp
+++ 563_tilt.cpp
@@ -1,14 +1,10 @@
 class Solution {
     int sum = 0;
-    int s(TreeNode* root) {
+    int h(TreeNode* root) {
         if (!root) return 0;
-        return root->val + s(root->right) + s(root->left);
-    }
-    void h(TreeNode* root) {
-        if (!root) return;
-        sum += abs(s(root->right) - s(root->left));
-        h(root->left);
-        h(root->right);
+        int left = h(root->left), right = h(root->right);
+        sum += abs(left - right);
+        return root->val + left + right;
     }
 public:
     int findTilt(TreeNode* root) {
