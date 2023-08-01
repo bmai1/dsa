b4fbc20525f1bb7b35826e3adb7c1092d86b9091
Brian Mai <0hrs3g@gmail.com>
2023-08-01 14:35:57 -0700

    more str.contains regex yay! kys.

--- pandas/1527_patients.py
+++ pandas/1527_patients.py
@@ -2,3 +2,4 @@ import pandas as pd
 
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
     return patients[patients['conditions'].str.contains('(^DIAB1)|( DIAB1)')]
+    # '(?:^|\s)DIAB1'
