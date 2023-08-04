bf3d55197009a873fa3e3d2c82e0d96be28aa666
Brian Mai <0hrs3g@gmail.com>
2023-08-03 22:53:20 -0700

    bad syntax

--- two pointers/905_arrparity.cpp
+++ two pointers/905_arrparity.cpp
@@ -7,12 +7,8 @@ public:
         // return ans;
 
         int j = 0; 
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] % 2 == 0) {
-                swap(nums[i], nums[j]);
-                ++j;
-            }
-        }
+        for (int i = 0; i < nums.size(); ++i) 
+            if (nums[i] % 2 == 0) swap(nums[i], nums[j++]);
         return nums;
     }
 };
