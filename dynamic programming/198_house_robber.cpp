4c2372b0a4b49d0c66664f27811692aad792b957
Brian Mai <0hrs3g@gmail.com>
2023-08-08 16:25:33 -0700

    more realistic iterative bottom-up memoization

--- dynamic programming/198_house_robber.cpp
+++ dynamic programming/198_house_robber.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        // iterative bottom-up memo
+        int n = nums.size();
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i + 1] = max(dp[i], dp[i - 1] + nums[i]);
+        }
+        return dp[n];
+
+
         // find max sum of elements without robbing adjacent houses
         // should not be every other house, the robbing house has to be higher than sum of two adjacent sides
 
