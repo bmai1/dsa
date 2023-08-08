59b6ccea269d1b0554401132838a4d9736c5b22d
Brian Mai <0hrs3g@gmail.com>
2023-08-08 01:49:05 -0700

    am i stupid?

--- backtracking/216_csum3.py
+++ backtracking/216_csum3.py
@@ -2,7 +2,6 @@ class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         combos = []
         combo = []
-
         def backtrack(combo, curr, start):
             if curr < 0 or len(combo) > k:
                 return
@@ -17,6 +16,6 @@ class Solution:
                 combo.append(i)
                 backtrack(combo, curr - i, start + 1)
                 combo.pop()
-            
+                
         backtrack(combo, n, 1)
         return combos
