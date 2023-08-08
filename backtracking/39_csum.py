7b3b7a395c9049ee9db66a580a4aa7672580be00
Brian Mai <0hrs3g@gmail.com>
2023-08-08 00:45:38 -0700

    backtrack while avoiding duplicates

---
+++ backtracking/39_csum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates = list(set(candidates))
+        candidates.sort()
+        ans = []
+
+        def backtrack(arr, curr, idx):
+            if (curr < 0):
+                return
+            if (curr == 0):
+                ans.append(arr.copy())
+                return
+            for i in range(idx, len(candidates)):
+                arr.append(candidates[i])
+                backtrack(arr, curr - candidates[i], i)
+                arr.pop()
+            
+        arr = []
+        backtrack(arr, target, 0)
+        return ans
