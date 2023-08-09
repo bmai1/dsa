f56c46d7612bf9ce58d79ac4f036fb03eb785e25
Brian Mai <0hrs3g@gmail.com>
2023-08-08 21:19:41 -0700

    & and <<= loop to count set bits

--- bit manip/338_countbits.cpp
+++ bit manip/338_countbits.cpp
@@ -11,7 +11,12 @@ public:
         for (int i = 0; i <= n; ++i) {
             int bits = 0, j = i;
             while (j != 0) {
+                // bitwise AND, only returns 1 if both 1, else 0
+                // this line increments bits whenever the last bit is one
+                // and converts the rest of the ones in j to zeroes.
                 bits += j & 1;
+                // right shift, divides an integer (left) by a power of 2 (right)
+                // a >>= b -> a / 2^b
                 j >>= 1;
             }
             ans.push_back(bits);
