76cc8a8b1c6348818ef2d65e2d21c90143386124
Brian Mai <0hrs3g@gmail.com>
2023-08-09 16:00:16 -0700

    mv

--- 1407_top_travel.sql
+++
@@ -1,5 +0,0 @@
-SELECT Users.name, IFNULL(SUM(Rides.distance), 0) AS travelled_distance
-FROM Users
-LEFT JOIN Rides ON Rides.user_id = Users.id
-GROUP BY Rides.user_id
-ORDER BY travelled_distance DESC, Users.name ASC
