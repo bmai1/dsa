685f1fe61bfb36c4eddbd79b7c59432e5ccf271b
Brian Mai <0hrs3g@gmail.com>
2023-08-09 15:19:47 -0700

    O((logn)^2) with bit manip, recursion

--- binary tree/222_cnt.cpp
+++ binary tree/222_cnt.cpp
@@ -1,15 +1,21 @@
 class Solution {
-private:
-    int depth(TreeNode* root, bool dir, int d) {
-        if (!root) return d;
-        if (dir) return depth(root->left, dir, d + 1);
-        return depth(root->right, dir, d + 1);
-    }
+// private:
+//     int depth(TreeNode* root, bool dir, int d) {
+//         if (!root) return d;
+//         if (dir) return depth(root->left, dir, d + 1);
+//         return depth(root->right, dir, d + 1);
+//     }
 public:
     int countNodes(TreeNode* root) {
-        int left =  depth(root, true, 0);
-        int right = depth(root, false, 0);
-        if (left == right) return pow(2, left) - 1;
+        // int left =  depth(root, true, 0);
+        // int right = depth(root, false, 0);
+
+        if (!root) return 0;
+        int lh = 0, rh = 0;
+        TreeNode *l = root, *r = root;
+        while (l) { ++lh; l = l->left; }
+        while (r) { ++rh; r = r->right; }
+        if (left == right) return (1 << lh) - 1;
         return countNodes(root->left) + countNodes(root->right) + 1;
     }
 };
