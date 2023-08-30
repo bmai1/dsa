a5ee3236c2ee7472e38ad174a94dacff7e675587
Brian Mai <0hrs3g@gmail.com>
2023-08-30 14:24:16 -0700

    math on cols from different tables

---
+++ sql/1251_avgp.sql
@@ -0,0 +1,5 @@
+SELECT a.product_id, ROUND(SUM(b.units * a.price) / SUM(b.units), 2) AS average_price
+FROM Prices a
+JOIN UnitsSold b
+ON a.product_id = b.product_id AND (b.purchase_date BETWEEN a.start_date AND a.end_date)
+GROUP BY product_id;
