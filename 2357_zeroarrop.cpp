e8f1aae985764454c12a20ecf11ca3242e14dc99
Brian Mai <0hrs3g@gmail.com>
2023-09-06 19:10:18 -0700

    brutal

---
+++ 2357_zeroarrop.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int op = 0, m;
+        while (true) {
+            m = INT_MAX;
+            for (auto n : nums) if (n > 0 && n < m) m = n;
+            for (auto &n : nums) if (n > 0) n -= m;
+            if (m == INT_MAX) break;
+            ++op;
+        }
+        return op;
+    }
+};
