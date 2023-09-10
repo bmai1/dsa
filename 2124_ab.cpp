2d71bac2bbb4488ac5af6a12a49a15362dfe912d
Brian Mai <0hrs3g@gmail.com>
2023-09-10 13:14:28 -0700

    one liner

--- 2124_ab.cpp
+++ 2124_ab.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool checkString(string s) {
+        return s.find("ba") == string::npos;
+        
         bool b = false; 
         for (char c : s) {
             if (b && c == 'a') return false;
