aff1977e8864cd6ddad063f59e8ec6e7087498d4
Brian Mai <0hrs3g@gmail.com>
2023-09-18 19:21:32 -0700

    select as, avg

---
+++ sql/1661_avgmachinetime.sql
@@ -0,0 +1,6 @@
+SELECT a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) AS processing_time
+FROM Activity a1 
+JOIN Activity a2
+ON a1.machine_id=a2.machine_id AND a1.process_id=a2.process_id
+AND a1.activity_type='start' AND a2.activity_type='end'
+GROUP BY a1.machine_id
