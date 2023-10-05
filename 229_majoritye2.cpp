848937dd61825c3bf4a192b7cf07658b1cea824c
Brian Mai <0hrs3g@gmail.com>
2023-10-05 08:49:43 -0700

    todo moore's voting algorithm

---
+++ 229_majoritye2.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int n = floor(nums.size() / 3.0);
+        unordered_map<int, int> freq;
+        for (int num : nums) freq[num]++;
+        for (auto it : freq) {
+            if (it.second > n) ans.push_back(it.first);
+        }
+        return ans;
+    }
+};
