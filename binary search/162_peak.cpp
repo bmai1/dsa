05d4daf5c04f91e2b50cecab113e100967a7a6f4
Brian Mai <0hrs3g@gmail.com>
2023-10-12 12:24:56 -0700

    much cleaner naive binary search

--- binary search/162_peak.cpp
+++ binary search/162_peak.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
+
+        int lo = 0, hi = nums.size() - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums[mid + 1]) hi = mid;
+            else lo = mid + 1; 
+        }
+        return lo;
+
         int n = nums.size();
         if (n == 1|| nums[0] > nums[1]) return 0;
         if (nums[n - 1] > nums[n - 2]) return n - 1;
