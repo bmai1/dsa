b1ce5b396a4ca7aa3851ae64dadd40f075496d15
Brian Mai <0hrs3g@gmail.com>
2023-11-06 09:26:43 -0800

    basic sql math, shortcut for conditional sum

--- sql/1211_queryquality.sql
+++ sql/1211_queryquality.sql
@@ -2,5 +2,6 @@ SELECT
 query_name, 
 ROUND(AVG(q.rating / q.position), 2) AS quality, 
 ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) / COUNT(*) * 100, 2) AS poor_query_percentage 
+-- SUM(rating < 3)
 FROM Queries AS q
 GROUP BY query_name
