3bd4469235be8b806d81db3221d3cb49d45c491b
Brian Mai <0hrs3g@gmail.com>
2023-11-20 12:55:44 -0800

    bidirectional graph from edges dfs

--- graph/1971_pathexists.cpp
+++ graph/1971_pathexists.cpp
@@ -4,17 +4,17 @@ private:
         if (visited[curr]) return false;
         if (curr == destination) return true;
         visited[curr] = true;
-        for (int i = 0; i < graph[curr].size(); ++i) {
-            if (dfs(graph, visited, graph[curr][i], destination)) return true;
+        for (auto node : graph[curr]) {
+            if (dfs(graph, visited, node, destination)) return true;
         }
         return false;
     } 
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         vector<vector<int>> graph(n);
-        for (int i = 0; i < edges.size(); ++i) {
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
+        for (auto edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
         }
         vector<bool> visited(n, false);
         return dfs(graph, visited, source, destination);
