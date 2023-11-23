c8030fa2bf01885b549e87c0eb039f3663a020bd
Brian Mai <0hrs3g@gmail.com>
2023-11-23 14:34:26 -0800

    self join on different col, round avg

---
+++ sql/1731_manageremp.sql
@@ -0,0 +1,10 @@
+SELECT 
+  m.employee_id, 
+  m.name,
+  COUNT(e.employee_id) AS reports_count,
+  ROUND(AVG(e.age)) AS average_age
+FROM Employees e
+JOIN Employees m
+ON e.reports_to = m.employee_id
+GROUP BY employee_id
+ORDER BY employee_id
