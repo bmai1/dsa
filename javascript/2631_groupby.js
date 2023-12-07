8e63c669a9b87d5ba8d03c2198340a2792025e82
Brian Mai <0hrs3g@gmail.com>
2023-12-07 10:17:17 -0800

    fuck javascript tbh

---
+++ javascript/2631_groupby.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+  return this.reduce((grouped, item) => {
+    const key = fn(item);
+    if (!grouped[key]) {
+      grouped[key] = [];
+    }
+    grouped[key].push(item);
+    return grouped;
+  }, {});
+};
+
+/**
+* [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+*/
