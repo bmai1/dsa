1f791ac7c571dc1a602ff10f006a3e5bb4a64eef
Brian Mai <0hrs3g@gmail.com>
2023-12-16 13:57:25 -0800

    clean up recursion

--- knights_tour/knights_tour.cpp
+++ knights_tour/knights_tour.cpp
@@ -14,7 +14,7 @@ private:
     vector<pair<int, int>> jumpOffset = {
         {-2, -1}, // 2 up, 1 left
         {-2, 1},  // 2 up, 1 right
-        {-1, 2},  // 1 up, 2 left
+        {-1, -2},  // 1 up, 2 left
         {-1, 2},  // 1 up, 2 right
         {1, -2},  // 1 down, 2 left
         {1, 2},   // 1 down, 2 right
@@ -23,35 +23,40 @@ private:
     };
 
     // check if a move is within board boundaries and not yet visited
-    bool isValid(int nextRow, int nextCol, int n, vector<vector<bool>>& visited, vector<int>& currPath) {
+    bool isValid(int nextRow, int nextCol, int n, vector<vector<bool>>& visited) {
         return (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n && !visited[nextRow][nextCol]);
     }
 
     // recursively try moves to find valid knight path
     void backtrack(vector<int>& currPath, vector<vector<bool>>& visited, int row, int col, int n) {
-        if (currPath.size() == n * n) path = currPath; // all squares have been visited
+        
         if (!path.empty()) return; // found valid path
+
+        currPath.push_back(row * n + col);
         visited[row][col] = true;
 
+        if (currPath.size() == n * n) {
+          path = currPath; // all squares have been visited
+          return;
+        }
+        
         int nextRow, nextCol;
         for (auto jump : jumpOffset) {
             // find next possible positions (maximum of 8) by looping through offset array
             nextRow = row + jump.first;
             nextCol = col + jump.second;
-            if (isValid(nextRow, nextCol, n, visited, currPath)) {
-                currPath.push_back(nextRow * n + nextCol); // 
+            if (isValid(nextRow, nextCol, n, visited)) {
                 backtrack(currPath, visited, nextRow, nextCol, n); // recursively test new path with current move
-                // backtrack 
-                visited[nextRow][nextCol] = false;
-                currPath.pop_back();
             }
         }
+        visited[row][col] = false;
+        currPath.pop_back();
     }
 public:
     // solver method
     vector<int> knightsTour(int n) {
         if (n < 5) return {}; 
-        vector<int> currPath = {0}; // arbitrary starting point
+        vector<int> currPath;
         vector<vector<bool>> visited(n, vector<bool>(n, false));
         backtrack(currPath, visited, 0, 0, n);
         return path;
@@ -60,7 +65,7 @@ public:
 
 int main() {
     Solution solve;
-    vector<int> res = solve.knightsTour(5);
+    vector<int> res = solve.knightsTour(8);
     for (int n : res) cout << n << " ";
     cout << endl << (res.empty() ? "No valid path exists." : "A valid path has been found.") << endl; 
     return 0;
