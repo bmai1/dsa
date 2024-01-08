85c87c002b6d641184c483f108a0fa2c49791bc6
Brian Mai <0hrs3g@gmail.com>
2024-01-07 16:44:56 -0800

    recursive traverse, sum on conditional

---
+++ binary tree/938_rangesum.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (!root) return 0;
+        int next = rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+        return (root->val >= low && root->val <= high) ? next + root->val : next;
+    }
+};
