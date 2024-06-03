90070fdf21e9c9f57c6d4f7a6e963281cbbd77af
Brian Mai <0hrs3g@gmail.com>
2024-06-03 12:12:03 -0700

    lol

--- 2486_appendss.cpp
+++ 2486_appendss.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        int i = 0, j = 0, cnt = 0;
-        while (j < t.size()) {
-            if (s[i] == t[j]) {
-                ++i, ++j;
-            }
-            else while (i < s.size() && s[i] != t[j]) ++i;
-            if (i == s.size() && j != t.size()) return t.size() - j;
-        }
-        return cnt;
+        // int i = 0, j = 0, cnt = 0;
+        // while (j < t.size()) {
+        //     if (s[i] == t[j]) {
+        //         ++i, ++j;
+        //     }
+        //     else while (i < s.size() && s[i] != t[j]) ++i;
+        //     if (i == s.size() && j != t.size()) return t.size() - j;
+        // }
+        // return cnt;
+
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) if (s[i++] == t[j]) ++j; 
+        return t.size() - j;
     }
 };
