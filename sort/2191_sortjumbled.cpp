f4cc5b66de574d0f05fde88ec320593fc08a2e51
Brian Mai <0hrs3g@gmail.com>
2024-07-23 23:50:07 -0700

    digit loop + stable sort

---
+++ sort/2191_sortjumbled.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        int n = nums.size();
+        vector<pair<int, int>> mapped;
+        for (int i = 0; i < n; ++i) {
+            int tmp = nums[i];
+            int val = tmp ? 0 : mapping[0];
+            int m = 1;
+            while (tmp) {
+                val = mapping[tmp % 10] * m + val;
+                m *= 10, tmp /= 10;
+            }
+            mapped.push_back({val, i});
+        }
+        // leaves equal values in same position
+        stable_sort(mapped.begin(), mapped.end());
+
+        vector<int> res;
+        for (auto& p : mapped) {
+            res.push_back(nums[p.second]);
+        }
+        return res;
+    }
+};
