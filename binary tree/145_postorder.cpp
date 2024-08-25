f6858112162f3e740da38a481c7feab08a65ed8b
Brian Mai <0hrs3g@gmail.com>
2024-08-25 15:26:03 -0700

    recursive postorder traversal

---
+++ binary tree/145_postorder.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    vector<int> res;
+    void pot(TreeNode* root) {
+        if (!root) return;
+        pot(root->left);
+        pot(root->right);
+        res.push_back(root->val);        
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        pot(root);
+        return res;
+    }
+};
